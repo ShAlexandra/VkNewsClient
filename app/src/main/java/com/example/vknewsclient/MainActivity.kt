8064a956c85b1c6646b80ee2afdbf4504449dbaa
AlexandraShulga <a.shulga@pbfgroup.ru>
2025-10-07 17:18:03 +0300

    InstagramProfileCard

--- app/src/main/java/com/example/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/example/vknewsclient/MainActivity.kt
@@ -3,7 +3,12 @@ package com.example.vknewsclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.vknewsclient.ui.theme.MainScreen
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.Modifier
+import com.example.vknewsclient.ui.theme.InstagramProfileCard
 import com.example.vknewsclient.ui.theme.VkNewsClientTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,7 +16,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             VkNewsClientTheme {
-                MainScreen()
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colors.background)
+                ) {
+                    InstagramProfileCard()
+                }
             }
         }
     }
---
+++ app/src/main/java/com/example/vknewsclient/ui/theme/InstagramProfileCard.kt
@@ -0,0 +1,103 @@
+package com.example.vknewsclient.ui.theme
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.vknewsclient.R
+
+@Composable
+fun InstagramProfileCard() {
+    Card(
+        modifier = Modifier.padding(8.dp),
+        backgroundColor = MaterialTheme.colors.background,
+        shape = RoundedCornerShape(
+            topStart = 4.dp,
+            topEnd = 4.dp
+        ),
+        border = BorderStroke(1.dp, MaterialTheme.colors.onBackground)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_instagram),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(60.dp)
+                        .clip(CircleShape)
+                        .background(Color.White)
+                        .padding(8.dp),
+                )
+                UserStatistics(title = "Posts", value = "6,950")
+                UserStatistics(title = "Followers", value = "436M")
+                UserStatistics(title = "Following", value = "76")
+            }
+            Text(
+                text = "Instagram",
+                fontSize = 32.sp,
+                fontFamily = FontFamily.Cursive
+            )
+            Text(
+                text = "#YoursToMake",
+                fontSize = 14.sp
+            )
+            Text(
+                text = "www.facebook.com/emotional_health",
+                fontSize = 14.sp
+            )
+            Button(onClick = {}) {
+                Text(text = "Follow")
+            }
+        }
+    }
+}
+
+@Composable
+private fun UserStatistics(
+    title: String,
+    value: String
+) {
+    Column(
+        modifier = Modifier.height(80.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+    ) {
+        Text(
+            text = value,
+            fontSize = 24.sp,
+            fontFamily = FontFamily.Cursive,
+        )
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
